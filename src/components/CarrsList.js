d07ecc41cbb98011b4d90535612110bf459045b8
Jorge989 <attiejorge89@gmail.com>
2022-01-31 16:12:45 -0300

    up

--- src/components/CarrsList.js
+++ src/components/CarrsList.js
@@ -4,11 +4,9 @@ import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 export default function CarrsList({ carros }) {
-  const [carrosImg, setCarroImg] = useState(carros.map((carro) => carro.image));
   if (carros.length === 0) {
     return <div>Nenhum carro econtrado...</div>;
   }
-  console.log(carrosImg);
 
   return (
     <div className="carros-list">
@@ -25,14 +23,14 @@ export default function CarrsList({ carros }) {
             {/* <img src={carro.image}></img> */}
             <Carousel>
               <div>
-                <img src={carro.image} />
+                <img src={carro.image} alt="image-1" />
                 {/* <p className="legend">Foto 1</p> */}
               </div>
               <div>
-                <img src={carro.image[1]} />
+                <img src={carro.image[1]} alt="image-2" />
               </div>
               <div>
-                <img src={carro.image[2]} />
+                <img src={carro.image[2]} alt="image-3" />
               </div>
             </Carousel>
             {/* <button className="rightbtn">{">"}</button> */}
